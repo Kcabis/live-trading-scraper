cd02b826c10e4ced7da844fb03b645af797418c3
codewithrajmangal <rajmangalrajak4@gmail.com>
2024-12-02 18:57:59 +0545

    changed

--- routes/web.php
+++ routes/web.php
@@ -82,13 +82,14 @@ Route::post('/add-stock', [StocksController::class, 'store']);
 
 
 use App\Http\Controllers\FolioadminController;
+Route::get('/admin', [FolioadminController::class, 'index']);
 Route::post('/add-ad',[FolioadminController::class,'store']);
-Route::get('/folioadmins', [FolioadminController::class, 'index'])->name('folioadmins.index');
 
-//Route::get('/folioadmins', [FolioadminController::class, 'index'])->name('folioadmins');
+
+// Route::get('/folioadmins', [FolioadminController::class, 'index'])->name('folioadmins');
 
 //use App\Http\Controllers\FolioadminController;
 
-//Route::get('/admin', [FolioadminController::class, 'index'])->name('admin.index');
+// Route::get('/admin', [FolioadminController::class, 'index'])->name('admin.index');
 //Route::post('/add-ad', [FolioadminController::class, 'store'])->name('admin.store');
 
